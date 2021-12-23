a32e0f7e9fa9122cc35018ef34f16b1ee43d7963
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-12-23 06:06:38 +0100

    Finish landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
+import NavigationBar from './NavigationBar';
+import Hero from './Hero';
 
 function App() {
   return (
+    <ChakraProvider>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <NavigationBar />
       </header>
+      <Hero />
     </div>
+    </ChakraProvider>
   );
 }
 
